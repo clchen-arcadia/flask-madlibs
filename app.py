c5e7931b9e530ec80d893af10ceea1ef41242d6e
trevorhudson <trevor.c.hudson@gmail.com>
2022-10-12 16:37:20 -0700

    finished part one of MadLib story

--- app.py
+++ app.py
@@ -14,12 +14,31 @@ def landing_page():
 
     prompts = silly_story.prompts
 
-    prompts = [word.replace("_", " ") for word in prompts]
-
     return render_template("questions.html", prompts = prompts)
 
 
 @app.get('/story')
 def answer_page():
-    for item in silly_story.generate(answer):
-        request.args.get()
+    ''' Read form inputs, generate new story on page'''
+    answers = {}
+
+    for word in silly_story.prompts:
+
+        answers[word] = request.args.get(word)
+
+    results = silly_story.generate(answers)
+
+    return render_template('results.html', results = results)
+
+
+
+
+
+
+
+
+
+
+
+
+
